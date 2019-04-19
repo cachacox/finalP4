1c8f7831bae917d8965fdd2d9326a0941fde0dd9
xcachaconx <cchacon_25@hotmail.com>
2019-04-19 13:38:05 -0600

    prueba

--- Proyecto Final/ProyectoFinalProgramacionIV/Logica/AccesoLogica.vb
+++ Proyecto Final/ProyectoFinalProgramacionIV/Logica/AccesoLogica.vb
@@ -3,6 +3,7 @@
 Public Class AccesoLogica
     Public Shared Function CargarExcel()
         Return AccesoDatos.CargarExcel()
+        'text
     End Function
 
     Public Shared Function InsertarInfoExcel(no_incidente, usuario, fecha_creacion, prioridad, prioridad_real, horas, fech_estado, fecha_asignado, fecha_resultado, estado) As DataTable
