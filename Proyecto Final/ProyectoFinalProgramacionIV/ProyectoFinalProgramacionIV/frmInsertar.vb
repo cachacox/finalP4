57ac18f99cb36e2ccf70ee2d1a5acfbd5e1d4a42
xcachaconx <cchacon_25@hotmail.com>
2019-04-21 17:52:07 -0600

    prioridades funcionando

--- Proyecto Final/ProyectoFinalProgramacionIV/ProyectoFinalProgramacionIV/frmInsertar.vb
+++ Proyecto Final/ProyectoFinalProgramacionIV/ProyectoFinalProgramacionIV/frmInsertar.vb
@@ -3,7 +3,7 @@ Public Class frmInsertar
     Private Sub frmInsertar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim x As DataTable
         Dim s As String
-        Dim sb As Integer = 0
+        Dim sb As Integer = 1
         dttFecha = Now()
         cbxPrioridad.SelectedIndex = 0
         cbxModulo.SelectedIndex = 0
@@ -19,7 +19,7 @@ Public Class frmInsertar
     Private Sub cbxPrioridad_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxPrioridad.SelectedIndexChanged
         Select Case cbxPrioridad.SelectedItem
             Case "Emergencia"
-                txtHoras.Text = "4"
+                txtHoras.Text = "4.5"
                 DeterminaHoras(cbxPrioridad.SelectedItem)
             Case "Alta"
                 txtHoras.Text = "26"
@@ -28,27 +28,16 @@ Public Class frmInsertar
                 txtHoras.Text = "32"
                 DeterminaHoras(cbxPrioridad.SelectedItem)
             Case "Baja"
-                txtHoras.Text = "288"
+                txtHoras.Text = "96"
                 DeterminaHoras(cbxPrioridad.SelectedItem)
         End Select
     End Sub
-    Public Shared Function horamilitar(_entero) As Double
-        Dim horam As Double = 0.0
-
-        If horam > 12 Then
-
-        End If
-
-        Return horam
-    End Function
-
-
-
     Sub DeterminaHoras(_str)
         Dim estimado As DateTime
         Dim _ano As Integer = Date.Today.Year
         Dim _mes As Integer = Date.Today.Month
         Dim _dia As Integer = Date.Today.Day
+        'Dim _temp As Integer = 23 'borrar esto, es para pruebas
         Dim _hora As Decimal = Math.Round(Now().Hour + (Now().Minute / 60), 2)
         Dim _fechas As Date = Today()
         Dim _flag As Boolean = False
@@ -73,8 +62,11 @@ Public Class frmInsertar
                     Dim startime As DateTimeOffset
                     Dim startOfShift As DateTimeOffset
                     Dim intDiff As Double
-                    startime = New DateTimeOffset(_ano, _mes, _dia, 0, 0, 0, DateTimeOffset.Now.Offset)
+                    startime = New DateTimeOffset(_ano, _mes, _dia, 0, 0, 0, DateTimeOffset.Now.Offset) 'volver a poner _dia
                     startOfShift = startime.AddHours(8)
+                    If _hora >= 16 And _fechas.DayOfWeek <> DayOfWeek.Saturday And _fechas.DayOfWeek <> DayOfWeek.Sunday Then
+                        startOfShift = startOfShift.AddDays(1)
+                    End If
                     Dim intHoras As Integer = 0
                     If _fechas.DayOfWeek = DayOfWeek.Saturday Or _fechas.DayOfWeek = DayOfWeek.Sunday Then
                         While (intHoras < 32)
@@ -87,18 +79,23 @@ Public Class frmInsertar
                         intLen = strFecha.Length - 7
                         txtFechaEstimada.Text = Strings.Left(strFecha, intLen)
                     Else
-                        'este if es si ya empezó el día, por ejemplo, el tiquete lo abren después de las 8
                         If _hora > startOfShift.Hour And _flag = False Then
                             intDiff = Math.Round((_hora - startOfShift.Hour), 2)
+                            If intDiff > 8 Then
+                                intDiff = intDiff - Int(intDiff)
+                            End If
                             _flag = True
                         End If
-                        'tengo que meter las horas de sab y dom
-                        'porque hay que contar sábado y domingo
                         While (intHoras < 32)
                             If startOfShift.Hour = 16 Then
                                 intHoras = intHoras + 8
                             End If
                             startOfShift = startOfShift.AddHours(8)
+                            If startOfShift.DayOfWeek = DayOfWeek.Saturday And intHoras < 32 Then
+                                startOfShift = startOfShift.AddDays(2)
+                            ElseIf startOfShift.DayOfWeek = DayOfWeek.Sunday And intHoras < 32 Then
+                                startOfShift = startOfShift.AddDays(1)
+                            End If
                         End While
                         If intDiff > 0 Then
                             strFecha = startOfShift.AddHours(8 + intDiff).ToString
@@ -110,9 +107,60 @@ Public Class frmInsertar
                         txtFechaEstimada.Text = Strings.Left(strFecha, intLen)
                     End If
                 Case "Baja"
-                    'falta
-                    estimado = dttFecha.AddHours(288)
-                    txtFechaEstimada.Text = estimado
+                    '96 horas
+                    Dim startime As DateTimeOffset
+                    Dim startOfShift As DateTimeOffset
+                    Dim intDiff As Double
+                    startime = New DateTimeOffset(_ano, _mes, _dia, 0, 0, 0, DateTimeOffset.Now.Offset) 'volver a poner _dia
+                    startOfShift = startime.AddHours(8)
+                    If _hora >= 16 And _fechas.DayOfWeek <> DayOfWeek.Saturday And _fechas.DayOfWeek <> DayOfWeek.Sunday Then
+                        startOfShift = startOfShift.AddDays(1)
+                    End If
+                    Dim intHoras As Integer = 0
+                    If _fechas.DayOfWeek = DayOfWeek.Saturday Or _fechas.DayOfWeek = DayOfWeek.Sunday Then
+                        While (intHoras < 96)
+                            If startOfShift.Hour = 16 Then
+                                intHoras = intHoras + 8
+                            End If
+                            startOfShift = startOfShift.AddHours(8)
+                            If startOfShift.DayOfWeek = DayOfWeek.Saturday And intHoras < 96 Then
+                                startOfShift = startOfShift.AddDays(2)
+                            ElseIf startOfShift.DayOfWeek = DayOfWeek.Sunday And intHoras < 96 Then
+                                startOfShift = startOfShift.AddDays(1)
+                            End If
+                        End While
+                        strFecha = startOfShift.AddHours(-8).ToString
+                        intLen = strFecha.Length - 7
+                        txtFechaEstimada.Text = Strings.Left(strFecha, intLen)
+                    Else
+                        'este if es si ya empezó el día, por ejemplo, el tiquete lo abren después de las 8
+                        If _hora > startOfShift.Hour And _flag = False Then
+                            intDiff = Math.Round((_hora - startOfShift.Hour), 2)
+                            If intDiff > 8 Then
+                                intDiff = intDiff - Int(intDiff)
+                            End If
+                            _flag = True
+                        End If
+                        While (intHoras < 96)
+                            If startOfShift.Hour = 16 Then
+                                intHoras = intHoras + 8
+                            End If
+                            startOfShift = startOfShift.AddHours(8)
+                            If startOfShift.DayOfWeek = DayOfWeek.Saturday And intHoras < 96 Then
+                                startOfShift = startOfShift.AddDays(2)
+                            ElseIf startOfShift.DayOfWeek = DayOfWeek.Sunday And intHoras < 96 Then
+                                startOfShift = startOfShift.AddDays(1)
+                            End If
+                        End While
+                        If intDiff > 0 Then
+                            strFecha = startOfShift.AddHours(8 + intDiff).ToString
+                            intLen = strFecha.Length - 7
+                        Else
+                            strFecha = startOfShift.AddHours(-8).ToString
+                            intLen = strFecha.Length - 7
+                        End If
+                        txtFechaEstimada.Text = Strings.Left(strFecha, intLen)
+                    End If
             End Select
         End If
     End Sub
