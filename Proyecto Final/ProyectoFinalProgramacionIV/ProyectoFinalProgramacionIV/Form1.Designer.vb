c3da22d0dc0ae4f43562a3b26d1a866b10fbae5c
xcachaconx <cchacon_25@hotmail.com>
2019-04-23 00:13:34 -0600

    cambios a form principal

--- Proyecto Final/ProyectoFinalProgramacionIV/ProyectoFinalProgramacionIV/Form1.Designer.vb
+++ Proyecto Final/ProyectoFinalProgramacionIV/ProyectoFinalProgramacionIV/Form1.Designer.vb
@@ -24,6 +24,11 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.PanelResumen = New System.Windows.Forms.Panel()
+        Me.LabelCargando = New System.Windows.Forms.Label()
+        Me.ProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.ButtonGuardarData = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ButtonCargarExcel = New System.Windows.Forms.Button()
         Me.Resumenes = New System.Windows.Forms.Label()
         Me.ButtonIncidentesResueltos = New System.Windows.Forms.Button()
         Me.ButtonIncidentesNuevos = New System.Windows.Forms.Button()
@@ -35,11 +40,8 @@ Partial Class Form1
         Me.ButtonEliminar = New System.Windows.Forms.Button()
         Me.ButtonModificar = New System.Windows.Forms.Button()
         Me.ButtonActualizar = New System.Windows.Forms.Button()
-        Me.ButtonCargarExcel = New System.Windows.Forms.Button()
-        Me.ButtonGuardarData = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.ProgressBar = New System.Windows.Forms.ProgressBar()
-        Me.LabelCargando = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.PanelResumen.SuspendLayout()
         CType(Me.DataGridViewPrincipal, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -64,6 +66,53 @@ Partial Class Form1
         Me.PanelResumen.Name = "PanelResumen"
         Me.PanelResumen.Size = New System.Drawing.Size(153, 422)
         Me.PanelResumen.TabIndex = 0
+        '
+        'LabelCargando
+        '
+        Me.LabelCargando.AutoSize = True
+        Me.LabelCargando.Location = New System.Drawing.Point(46, 113)
+        Me.LabelCargando.Name = "LabelCargando"
+        Me.LabelCargando.Size = New System.Drawing.Size(53, 13)
+        Me.LabelCargando.TabIndex = 8
+        Me.LabelCargando.Text = "Cargando"
+        '
+        'ProgressBar
+        '
+        Me.ProgressBar.Location = New System.Drawing.Point(10, 132)
+        Me.ProgressBar.Name = "ProgressBar"
+        Me.ProgressBar.Size = New System.Drawing.Size(131, 23)
+        Me.ProgressBar.TabIndex = 6
+        '
+        'ButtonGuardarData
+        '
+        Me.ButtonGuardarData.Location = New System.Drawing.Point(10, 74)
+        Me.ButtonGuardarData.Name = "ButtonGuardarData"
+        Me.ButtonGuardarData.Size = New System.Drawing.Size(131, 23)
+        Me.ButtonGuardarData.TabIndex = 7
+        Me.ButtonGuardarData.Text = "Guardar en SQL"
+        Me.ButtonGuardarData.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(33, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Comandos"
+        '
+        'ButtonCargarExcel
+        '
+        Me.ButtonCargarExcel.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonCargarExcel.Location = New System.Drawing.Point(10, 36)
+        Me.ButtonCargarExcel.Name = "ButtonCargarExcel"
+        Me.ButtonCargarExcel.Size = New System.Drawing.Size(131, 23)
+        Me.ButtonCargarExcel.TabIndex = 6
+        Me.ButtonCargarExcel.Text = "Importar Excel"
+        Me.ButtonCargarExcel.UseVisualStyleBackColor = True
         '
         'Resumenes
         '
@@ -175,58 +224,31 @@ Partial Class Form1
         Me.ButtonActualizar.Text = "Actualizar"
         Me.ButtonActualizar.UseVisualStyleBackColor = True
         '
-        'ButtonCargarExcel
+        'Label2
         '
-        Me.ButtonCargarExcel.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.ButtonCargarExcel.Location = New System.Drawing.Point(10, 36)
-        Me.ButtonCargarExcel.Name = "ButtonCargarExcel"
-        Me.ButtonCargarExcel.Size = New System.Drawing.Size(131, 23)
-        Me.ButtonCargarExcel.TabIndex = 6
-        Me.ButtonCargarExcel.Text = "Importar Excel"
-        Me.ButtonCargarExcel.UseVisualStyleBackColor = True
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Label2"
         '
-        'ButtonGuardarData
+        'Label3
         '
-        Me.ButtonGuardarData.Location = New System.Drawing.Point(10, 74)
-        Me.ButtonGuardarData.Name = "ButtonGuardarData"
-        Me.ButtonGuardarData.Size = New System.Drawing.Size(131, 23)
-        Me.ButtonGuardarData.TabIndex = 7
-        Me.ButtonGuardarData.Text = "Guardar en SQL"
-        Me.ButtonGuardarData.UseVisualStyleBackColor = True
-        '
-        'Label1
-        '
-        Me.Label1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(33, 5)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(83, 17)
-        Me.Label1.TabIndex = 6
-        Me.Label1.Text = "Comandos"
-        '
-        'ProgressBar
-        '
-        Me.ProgressBar.Location = New System.Drawing.Point(10, 132)
-        Me.ProgressBar.Name = "ProgressBar"
-        Me.ProgressBar.Size = New System.Drawing.Size(131, 23)
-        Me.ProgressBar.TabIndex = 6
-        '
-        'LabelCargando
-        '
-        Me.LabelCargando.AutoSize = True
-        Me.LabelCargando.Location = New System.Drawing.Point(46, 113)
-        Me.LabelCargando.Name = "LabelCargando"
-        Me.LabelCargando.Size = New System.Drawing.Size(53, 13)
-        Me.LabelCargando.TabIndex = 8
-        Me.LabelCargando.Text = "Cargando"
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 40)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Label3"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(832, 427)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.ButtonActualizar)
         Me.Controls.Add(Me.ButtonModificar)
         Me.Controls.Add(Me.ButtonEliminar)
@@ -241,6 +263,7 @@ Partial Class Form1
         Me.PanelResumen.PerformLayout()
         CType(Me.DataGridViewPrincipal, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -261,4 +284,6 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents ProgressBar As ProgressBar
     Friend WithEvents LabelCargando As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
 End Class
